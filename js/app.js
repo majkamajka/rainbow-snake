05d6b2989b0c179291dd4c0e03e12f98ffcf2181
Majka Karpinska <maja.miri@gmail.com>
2017-05-27 21:48:09 +0200

    cat and face added, bigger cage!

--- js/app.js
+++ js/app.js
@@ -2,27 +2,27 @@ document.addEventListener("DOMContentLoaded", function(){
 
   var pix = document.querySelectorAll(".pixel");
 
-  // function white (element) {
-  //
-  //   element.style.backgroundColor = "white";
-  // }
-
   function randomColor () {
     var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
     this.style.backgroundColor = randomColor;
+    this.style.backgroundImage = "url('images/face.png')"
     var dupa = this;
     setTimeout(function white () {
       dupa.style.backgroundColor = "white";
+      dupa.style.backgroundImage = "none";
     }, 500);
-
   };
 
+  function hideFace () {
+    this.style.backgroundImage = "none";
+  }
 
 
   for (var i = 0; i < pix.length; i++) {
     pix[i].addEventListener("mouseover", randomColor);
+    pix[i].addEventListener("mouseout", hideFace);
   }
 
-
-
 });
+
+// IDEA: add eyes and face to the snake head. -> hover, or add element
